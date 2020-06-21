a6270672cb87114560db122a872960f19fa41509
David Antaki <antakidavid@gmail.com>
2020-06-20 20:40:24 -0400

    Updated header

--- TwoPtCalCurve.h
+++ TwoPtCalCurve.h
@@ -3,8 +3,46 @@
 // Date: 6/20/2020
 // License: This software is not open source and is copyrighted by David
 // Antaki and Sous Bois International, Inc.
-// Contents: Library for a calibration curve for use with a sensor,
-// for example.
+// Contents: Library for a calibration curve for use with a sensor.
+//
+// A calibration curve is a function/graph with the theoretical values that
+// the sensor SHOULD/IS EXPECTED to output on the y-axis, and the ACTUALLY
+// values measured by the sensor on the x-axis.
+//
+// For example, if you had a temperature sensor. To calibrate it, you
+// read a value from the temperature sensor and say it outputs 30F but
+// the actual temperature in the room is 32F. (30, 32) becomes your first
+// point on the calibration curve.
+//
+// On a different day (or you change the environment temperature), you take
+// another reading from the temp sensor and it reads 54F, but the actual
+// temp in the room is 51F. (54, 51) becomes your second point on the
+// calibration curve.
+//
+// (30, 32) and (54, 51) would be the CalPoint's you pass when instantiating
+// an object of this class.
+
+// MIT License
+
+// Copyright (c) 2020 David Antaki
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef TWO_PT_CAL_CURVE_H
 #define TWO_PT_CAL_CURVE_H
@@ -28,6 +66,12 @@ public:
     } CalCurve;
 
     TwoPtCalCurve();
+
+    // If you picture the calibration curve graph, it should be the theoretical
+    // output of the sensor graphs vs the values actually measured by the sensor.
+    //
+    // @param _calPt1   The first point on the calibration curve.
+    // @param _calPt2   The second point on the calibration curve.
     TwoPtCalCurve(CalPoint _calPt1, CalPoint _calPt2);
     ~TwoPtCalCurve();
     // To calibrate an input value/point using the 2-pt calibration curve.
